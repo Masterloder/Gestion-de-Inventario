41b5ac181866baa4122fd4d421a74046d04bf3ff
Yefferson <masterloder@gmail.com>
2025-10-31 17:50:49 -0400

    Renovacion_de_welcome

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,21 @@
-    <section>
-        <section class="logo"> >
-        <h1> <img src="{{ asset('images/Logos/Logo_corvisucre.png') }}" alt="Logo" class="img-fluid rounded-circle"> Corvisucre</h1>
-        </section>
-        <nav>
-            <a href="/Inicio_de_sesion">Iniciar sesión</a>
-            <a href="/registro">Registrarse</a>
-            <a href="/productos">Productos</a>
-            <a href="/contacto">Contacto</a>
-        </nav>
-    </section>
+
+<nav class="navbar navbar-expand navbar-primary bg-primary">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="/"> <img src="{{ asset('images/Logos/Logo_corvisucre.png') }}" width="90" height="50" class="d-inline-block align-left img-fluid " alt="Logo"> Corvisucre</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav justify-content-end mr-auto mt-2 mt-lg-0 align-right">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Bienvenido </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Inicio_de_sesion">Inicio de sesión</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/registro">Registrarse</a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/CSS/Header.css">
     <title>Gestion de Inventario</title>
 </head>
 <body>
-    <header>   
+    <header>
         @include('components.header')
-
     </header>
-    <h1>Bienvenido a la Gestion de Inventario</h1>
-    <p>Esta es la página de bienvenida de la aplicación de gestión de inventario.</p>
     @if(session('success'))
     <div class="alert alert-success" role="alert">
         {{ session('success') }}
     </div>
     @endif
-        <p>Iniciar sesión</p>
-        <a href="/Inicio_de_sesion"><button type="button">Iniciar sesión</button></a>
-        <p>Registrarse</p>
-        <a href="/registro"><button type="button">Registrarse</button></a>
-
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+    <div>
+        @include('components.carrucel')
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
 </html>
