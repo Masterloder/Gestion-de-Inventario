b91abf0dd7d65ac5fa9835760e682642fd7b7589
Yefferson <masterloder@gmail.com>
2025-10-31 13:45:02 -0400

    Modificacion_del_welcome

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+    <section>
+        <section class="logo"> >
+        <h1> <img src="{{ asset('images/Logos/Logo_corvisucre.png') }}" alt="Logo" class="img-fluid rounded-circle"> Corvisucre</h1>
+        </section>
+        <nav>
+            <a href="/Inicio_de_sesion">Iniciar sesión</a>
+            <a href="/registro">Registrarse</a>
+            <a href="/productos">Productos</a>
+            <a href="/contacto">Contacto</a>
+        </nav>
+    </section>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/dashboard', function () {
 Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
-Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
 Route::get('dashboard', [AuthController::class, 'dashboard']);
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
